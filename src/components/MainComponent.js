4bb58e4d84fe19ff23fed4729c4f4cd359b7302f
Yatish Chandra Pitta <ycchandra29@gmail.com>
2020-05-21 23:32:23 -0400

    Single Page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,9 +3,13 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes'
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import '../App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -15,7 +19,10 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            selectedDish: null,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -25,7 +32,15 @@ class Main extends Component {
 
     render() {
 
-        const HomePage = () => { return (<Home />); }
+        const HomePage = () => {
+            return (<Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
+
+            );
+        }
 
         return (
             <div>
@@ -33,7 +48,8 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-                    <Redirect to="/home"/>
+                    <Route exact path="/contactus" component={Contact} />
+                    <Redirect to="/home" />
                 </Switch>
                 <Footer />
                 {/* <header className="App-header">
